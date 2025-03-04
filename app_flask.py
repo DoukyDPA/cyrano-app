021e597369104be43a633c10fc42ba6b4284a569
DoukyDPA <daniel.p.angelini@gmail.com>
2025-03-04 17:28:29 +0100

    Update app_flask.py

--- app_flask.py
+++ app_flask.py
@@ -1,3 +1,27 @@
+# Configuration de session pour Render.com
+from flask.sessions import SecureCookieSessionInterface
+
+class CustomSessionInterface(SecureCookieSessionInterface):
+    def save_session(self, app, session, response):
+        domain = self.get_cookie_domain(app)
+        path = self.get_cookie_path(app)
+        
+        if not session:
+            if session.modified:
+                response.delete_cookie(app.session_cookie_name,
+                                      domain=domain, path=path)
+            return
+            
+        httponly = self.get_cookie_httponly(app)
+        secure = self.get_cookie_secure(app)
+        expires = self.get_expiration_time(app, session)
+        val = self.get_signing_serializer(app).dumps(dict(session))
+        response.set_cookie(app.session_cookie_name, val,
+                          expires=expires, httponly=httponly,
+                          domain=domain, path=path, secure=secure, samesite='None')
+
+app.session_interface = CustomSessionInterface()
+
 import os
 import requests
 import logging
